41bb0c5ef6ba43fac3d855628de928ea6c6b432a
Mathew Hartley <matt@route66.sytes.net>
2013-08-16 11:26:32 +1200

    Trying different ports to see if it fixes travis build error

--- start_test.js
+++ start_test.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn;
 
-var meteor = spawn('./meteor/meteor', ['test-packages', '--driver-package', 'test-in-console', '-p', 10012, './']);
+var meteor = spawn('./meteor/meteor', ['test-packages', '--driver-package', 'test-in-console', '-p', 10213, './']);
 meteor.stdout.pipe(process.stdout);
 meteor.stderr.pipe(process.stderr);
 
 meteor.stdout.on('data', function startTesting(data) {
   var data = data.toString();
-  if(data.match(/10012/)) {
+  if(data.match(/10213/)) {
     console.log('starting testing...');
     meteor.stdout.removeListener('data', startTesting);
     runTestSuite();
-  } 
+  }
 });
 
 function runTestSuite() {
-  process.env.URL = "http://localhost:10012/"
+  process.env.URL = "http://localhost:10213/";
   var phantomjs = spawn('phantomjs', ['./meteor/packages/test-in-console/runner.js']);
   phantomjs.stdout.pipe(process.stdout);
   phantomjs.stderr.pipe(process.stderr);
