d9c57d4b64d9091da3c85c7c7e9dbd0d1dfcad4c
Mathew Hartley <matt@route66.sytes.net>
2014-05-12 10:05:09 +1200

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  summary: "Style with attitude."
+  summary: 'Style with attitude.'
 });
 
 Package._transitional_registerBuildPlugin({
-  name: "meteor-scss",
+  name: 'meteor-scss',
   use: [],
   sources: [
     'plugin/compile-scss.js'
   ],
   npmDependencies: {
-    "node-sass": "0.8.4",
-    "lodash": "2.4.1"
+    'node-sass': '0.8.4',
+    'lodash': '2.4.1'
   }
 });
 
