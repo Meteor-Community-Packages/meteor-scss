ba753b5c9acca00b6fdcbb41dedc31ef57f9e47e
Mathew Hartley <me@mathewhartley.com>
2014-12-25 22:22:22 +1300

    Prepare for 2.0.0 of node-sass (note, maps are very broken)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Style with attitude and autoprefixer.',
-  version: "1.0.0",
+  version: "2.0.0-beta",
   git: "https://github.com/fourseven/meteor-scss.git"
 });
 
@@ -10,7 +10,7 @@ Package._transitional_registerBuildPlugin({
     'plugin/compile-scss.js'
   ],
   npmDependencies: {
-    'node-sass': '1.1.4',
+    'node-sass': 'v2.0.0-beta',
     'lodash': '2.4.1',
     'autoprefixer-core': '3.1.0',
   }
--- plugin/compile-scss.js
+++ plugin/compile-scss.js
@@ -3,6 +3,7 @@ var sass = Npm.require('node-sass');
 var fs = Npm.require('fs');
 var _ = Npm.require('lodash');
 var autoprefixer = Npm.require('autoprefixer-core');
+var Future = Npm.require('fibers/future');
 
 var loadJSONFile = function (filePath) {
   var content = fs.readFileSync(filePath);
@@ -24,16 +25,21 @@ var sourceHandler = function(compileStep) {
 
   var optionsFile = path.join(process.cwd(), 'scss.json');
   var scssOptions = {};
+  var sourceMap   = null;
+  var future = new Future;
 
   if (fs.existsSync(optionsFile)) {
     scssOptions = loadJSONFile(optionsFile);
   }
 
   var options = _.extend({}, scssOptions, {
-    sourceComments: 'none'
+    sourceMap:         true,
+    sourceMapEmbed:    true,
+    sourceMapContents: true,
+    outputStyle:       'compressed',
   });
 
-  options.file = compileStep._fullInputPath;
+  options.file  = compileStep._fullInputPath;
 
   if (!_.isArray(options.includePaths)) {
     options.includePaths = [options.includePaths];
@@ -41,9 +47,47 @@ var sourceHandler = function(compileStep) {
 
   options.includePaths = options.includePaths.concat(path.dirname(compileStep._fullInputPath));
 
-  var css;
+  options.success = function (css) {
+    if (options.enableAutoprefixer
+       || (compileStep.fileOptions && compileStep.fileOptions.isTest)) {
+      var autoprefixerOptions = options.autoprefixerOptions || {};
+
+      try {
+        // Applying Autoprefixer to compiled css
+        var processor = autoprefixer(autoprefixerOptions);
+        css = processor.process(css).css;
+      } catch (e) {
+        compileStep.error({
+          message: "Autoprefixer error: " + e,
+          sourcePath: e.filename || compileStep.inputPath
+        });
+        return future.error(e);
+      }
+    }
+    console.log(css);
+    if (options.sourceComments !== 'none') {
+      // sourceMap = JSON.parse(css.sourceMap);
+      // delete sourceMap.file;
+      // sourceMap.file = compileStep.pathForSourceMap;
+      // sourceMap.sources = [compileStep.inputPath];
+      // sourceMap.sourcesContent = [compileStep.read().toString('utf8')];
+    }
+
+    compileStep.addStylesheet({
+      path: compileStep.inputPath + ".css",
+      data: css.css
+      // sourceMap: JSON.stringify(sourceMap)
+    });
+    future.return(null);
+  }
+  options.error = function (error) {
+    return  future.error(error);
+  }
+
+
+
   try {
-    css = sass.renderSync(options);
+    sass.render(options);
   } catch (e) {
     // less.render() is supposed to report any errors via its
     // callback. But sometimes, it throws them instead. This is
@@ -56,28 +100,7 @@ var sourceHandler = function(compileStep) {
     });
     return;
   }
-
-  if (options.enableAutoprefixer
-     || (compileStep.fileOptions && compileStep.fileOptions.isTest)) {
-    var autoprefixerOptions = options.autoprefixerOptions || {};
-
-    try {
-      // Applying Autoprefixer to compiled css
-      var processor = autoprefixer(autoprefixerOptions);
-      css = processor.process(css).css;
-    } catch (e) {
-      compileStep.error({
-        message: "Autoprefixer error: " + e,
-        sourcePath: e.filename || compileStep.inputPath
-      });
-      return;
-    }
-  }
-
-  compileStep.addStylesheet({
-    path: compileStep.inputPath + ".css",
-    data: css
-  });
+  return future.wait();
 }
 
 Plugin.registerSourceHandler("scss", {archMatching: 'web'}, sourceHandler);
