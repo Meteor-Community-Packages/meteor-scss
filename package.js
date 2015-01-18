32c83ea7e31976187f32b5d1776452ee5ac72c31
Mathew Hartley <me@mathewhartley.com>
2015-01-19 12:21:18 +1300

    Prep for 1.2.x release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Style with attitude and autoprefixer.',
-  version: "1.0.0",
+  version: "1.2.3",
   git: "https://github.com/fourseven/meteor-scss.git"
 });
 
@@ -10,7 +10,7 @@ Package._transitional_registerBuildPlugin({
     'plugin/compile-scss.js'
   ],
   npmDependencies: {
-    'node-sass': '1.1.4',
+    'node-sass': '1.2.3',
     'lodash': '2.4.1',
     'autoprefixer-core': '3.1.0',
   }
