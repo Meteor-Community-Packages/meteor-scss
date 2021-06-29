f22420d1794d55b68f4c7df2cbf9394ab2a90ee8
Jan Dvorak <storyteller@freedombase.net>
2021-06-29 22:26:35 +0200

    Update node-sass, dependencies & travis node version

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   summary: 'Style with attitude. Sass and SCSS support for Meteor.js.',
-  version: '4.14.2',
+  version: '4.15.0',
   git: 'https://github.com/fourseven/meteor-scss.git',
   name: 'fourseven:scss',
 });
 
 Package.registerBuildPlugin({
   name: 'compileScssBatch',
-  use: ['caching-compiler@1.2.0', 'ecmascript@0.12.0'],
+  use: ['caching-compiler@1.2.2', 'ecmascript@0.15.1'],
   sources: ['plugin/compile-scss.js'],
   npmDependencies: {
-    'node-sass': '4.14.1',
+    'node-sass': '5.0.0',
   },
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8');
+  api.versionsFrom('1.9');
   api.use('isobuild:compiler-plugin@1.0.0');
 });
 
