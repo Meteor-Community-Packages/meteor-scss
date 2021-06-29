c0faa697ca25ddc6bc21eb92cb459bf0c589baa1
Jan Dvorak <storyteller@freedombase.net>
2021-06-29 22:45:19 +0200

    Bump node-sass to the latest version

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.registerBuildPlugin({
   use: ['caching-compiler@1.2.2', 'ecmascript@0.15.1'],
   sources: ['plugin/compile-scss.js'],
   npmDependencies: {
-    'node-sass': '5.0.0',
+    'node-sass': '6.0.1',
   },
 });
 
