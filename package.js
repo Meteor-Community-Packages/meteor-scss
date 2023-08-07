eddd3d6efbbb8e91700ccc23d838ce4f2c31e423
Igor Loskutov <igor.loskutoff@gmail.com>
2023-08-07 15:23:15 +0700

    sass 1.64.2

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.registerBuildPlugin({
   use: ['caching-compiler@1.2.2', 'ecmascript@0.15.1'],
   sources: ['plugin/compile-scss.js'],
   npmDependencies: {
-    'sass': '1.57.1',
+    'sass': '1.64.2',
   },
 });
 
